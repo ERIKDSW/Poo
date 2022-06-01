bbbc0e318831e7b27f90d459f9e031f781663456
Erik Alejandro Olvera Flores <105739861+ERIKDSW@users.noreply.github.com>
2022-06-01 13:06:29 -0500

    Delete TALLERPOO-master directory

--- TALLERPOO-master/arte/data.js
+++
@@ -1,8 +0,0 @@
-export const data = [
-  {
-      question: "¿Quien pinto la monalisa?",
-      choices: ["Leonardo Da Vinci", "Picasso", "Monet", "Vicent Van Goh", 'Frida Khalo', 'Diego Rivera'],
-      answer: "Leonardo Da Vinci",
-    },
-    
-  ];
